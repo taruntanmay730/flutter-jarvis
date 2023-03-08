6aa88650f70b18caf9b7d9393f5068eb98428088
Tarun Tanmay <90436309+taruntanmay730@users.noreply.github.com>
2023-03-08 13:39:34 +0530

    local update

---
+++ lib/resources/NativeCommunication.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class NativeCommunication extends StatefulWidget {
+  const NativeCommunication({Key? key}) : super(key: key);
+
+  @override
+  State<NativeCommunication> createState() => _NativeCommunicationState();
+}
+
+class _NativeCommunicationState extends State<NativeCommunication> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
